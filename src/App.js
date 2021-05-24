fb5f8f3ced2cdb5687796ac04e8b98c57dfbbf61
Aditya Chandeliya <adityakumar@giddh.com>
2021-05-24 15:39:41 +0530

    chandeliya | modifying App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fName = 'Wolf';
+  const lName = 'Gupta';
+  const age = 9;
+  const job = 'Software Engineer';
+  const company = 'Google Inc';
+  const Yearlypckg = '1.2 Crore';
+  const education = 'WhiteHat Junior';
+
+  const getFullName = (fName, lName) => {
+    return `${fName} ${lName}`;
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Full Name: {getFullName(fName, lName)}</h3>
+      <p>Age: {age} years</p>
+      <p>Education: {education}</p>
+      <p>Job: {job} at {company}</p>
+      <p>Annual Salary: {Yearlypckg}</p>
     </div>
   );
 }
