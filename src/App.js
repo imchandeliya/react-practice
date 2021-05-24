a168a2172265ea7d92ef0633ba784ae459d25cf1
Aditya Chandeliya <adityakumar@giddh.com>
2021-05-24 20:21:42 +0530

    added styles

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 
 function App() {
 
-  const blogObject = {
-    title: 'Lorem Ipsum',
-    content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet.'
-  };
+    const blogObject = {
+        title: 'Lorem Ipsum',
+        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet.'
+    };
 
-  return (
+    const styles = {
+        margin: '16px',
+        padding: '16px',
+        boxSizing: 'border-box',
+        borderRadius: '5px',
+        boxShadow: '0 2px 5px #ccc'
 
-    React.createElement("div", {
-      className: "App"
-    },
-      React.createElement("div", null,
-        React.createElement("h3", null, blogObject.title),
-        React.createElement("p", null, blogObject.content)),
-      React.createElement("hr", null),
-      React.createElement("div", null,
-        React.createElement("h3", null, blogObject.title),
-        React.createElement("p", null, blogObject.content)),
-      React.createElement("hr", null),
-      React.createElement("div", null,
-        React.createElement("h3", null, blogObject.title),
-        React.createElement("p", null, blogObject.content)))
-  );
+    };
+
+    return (
+        <div className='App'>
+            <div className='BlogCard'>
+                <h3>{blogObject.title}</h3>
+                <p>{blogObject.content}</p>
+            </div>
+            <div className='BlogCard'>
+                <h3>{blogObject.title}</h3>
+                <p>{blogObject.content}</p>
+            </div>
+            <div className='BlogCard'>
+                <h3>{blogObject.title}</h3>
+                <p>{blogObject.content}</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
