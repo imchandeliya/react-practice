92d63a4cca49d45e146f6a55b27737527e007456
Aditya Chandeliya <adityakumar@giddh.com>
2021-05-24 20:36:28 +0530

    fetching data dynamically

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-    const blogObject = {
-        title: 'Lorem Ipsum',
-        content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet.'
-    };
+    const blogArr = [
+        {
+            id: 1,
+            title: 'Blog Title 1',
+            content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet.'
+        },
+        {
+            id: 2,
+            title: 'Blog Title 2',
+            content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet.'
+        },
+        {
+            id: 3,
+            title: 'Blog Title 3',
+            content: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem ipsum dolor sit amet.'
+        }
+    ]
 
-    const styles = {
-        margin: '16px',
-        padding: '16px',
-        boxSizing: 'border-box',
-        borderRadius: '5px',
-        boxShadow: '0 2px 5px #ccc'
+    const blogCards = blogArr.map((item) => {
+        console.log(item);
 
-    };
+        return (
+            <div className='BlogCard' key={item.id}>
+                <h3>{item.title}</h3>
+                <p>{item.content}</p>
+            </div>
+        );
+    });
+
 
     return (
         <div className='App'>
-            <div className='BlogCard'>
-                <h3>{blogObject.title}</h3>
-                <p>{blogObject.content}</p>
-            </div>
-            <div className='BlogCard'>
-                <h3>{blogObject.title}</h3>
-                <p>{blogObject.content}</p>
-            </div>
-            <div className='BlogCard'>
-                <h3>{blogObject.title}</h3>
-                <p>{blogObject.content}</p>
-            </div>
+            {blogCards}
         </div>
     );
 }
